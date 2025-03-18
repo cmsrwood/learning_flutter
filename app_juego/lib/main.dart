e9c2b1f69454a566b8180924d15ce5d7f5728076
cmsrwood <dilanfantas@gmail.com>
2025-03-17 23:59:37 -0500

    App de juego completa

--- app_juego/lib/main.dart
+++ app_juego/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MainApp());
@@ -11,11 +12,84 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Juego',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      title: 'Casino',
+      home: CasinoPage(),
+    );
+  }
+}
+
+class CasinoPage extends StatefulWidget {
+  const CasinoPage({super.key});
+
+  @override
+  State<CasinoPage> createState() => _CasinoPageState();
+}
+
+class _CasinoPageState extends State<CasinoPage> {
+  String ruta1 = 'assets/images/interrogacion.jpg';
+  String ruta2 = 'assets/images/interrogacion.jpg';
+  String ruta3 = 'assets/images/interrogacion.jpg';
+  String mensaje = '';
+
+  void jugar() {
+    setState(() {
+      List<String> rutas = [
+        'assets/images/feliz.jpg',
+        'assets/images/seria.jpg',
+        'assets/images/triste.jpg',
+      ];
+      ruta1 = rutas[math.Random().nextInt(rutas.length)];
+      ruta2 = rutas[math.Random().nextInt(rutas.length)];
+      ruta3 = rutas[math.Random().nextInt(rutas.length)];
+    });
+
+    if (ruta1 == ruta2 && ruta2 == ruta3) {
+      setState(() {
+        mensaje = 'Ganaste! Felicidades';
+      });
+    } else {
+      setState(() {
+        mensaje = 'Perdiste, vuelve a intentarlo';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Casino', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 50),
+          const Text(
+            'Gana o Pierde',
+            style: TextStyle(color: Colors.red, fontSize: 24),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(ruta1, width: 100, fit: BoxFit.cover),
+              Image.asset(ruta2, width: 100, fit: BoxFit.cover),
+              Image.asset(ruta3, width: 100, fit: BoxFit.cover),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Text(mensaje, style: TextStyle(color: Colors.red, fontSize: 24)),
+          ElevatedButton(
+            onPressed: jugar,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              elevation: 3,
+            ),
+            child: const Text('Jugar'),
+          ),
+        ],
       ),
     );
   }
