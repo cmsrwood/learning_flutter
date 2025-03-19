1256028397a7ebf644b20df06d677370cb877805
cmsrwood <dilanfantas@gmail.com>
2025-03-18 19:05:51 -0500

    animaciones para ruleta

--- app_juego/lib/main.dart
+++ app_juego/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+import 'dart:async';
 
 void main() {
   runApp(const MainApp());
@@ -30,28 +31,37 @@ class _CasinoPageState extends State<CasinoPage> {
   String ruta2 = 'assets/images/interrogacion.jpg';
   String ruta3 = 'assets/images/interrogacion.jpg';
   String mensaje = '';
+  bool spinning = false;
 
   void jugar() {
-    setState(() {
-      List<String> rutas = [
-        'assets/images/feliz.jpg',
-        'assets/images/seria.jpg',
-        'assets/images/triste.jpg',
-      ];
-      ruta1 = rutas[math.Random().nextInt(rutas.length)];
-      ruta2 = rutas[math.Random().nextInt(rutas.length)];
-      ruta3 = rutas[math.Random().nextInt(rutas.length)];
-    });
-
-    if (ruta1 == ruta2 && ruta2 == ruta3) {
-      setState(() {
-        mensaje = 'Ganaste! Felicidades';
-      });
-    } else {
-      setState(() {
-        mensaje = 'Perdiste, vuelve a intentarlo';
-      });
+    if (spinning) {
+      return;
     }
+    spinning = true;
+    int spins = 0;
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
+      setState(() {
+        List<String> rutas = [
+          'assets/images/feliz.jpg',
+          'assets/images/seria.jpg',
+          'assets/images/triste.jpg',
+        ];
+        ruta1 = rutas[math.Random().nextInt(rutas.length)];
+        ruta2 = rutas[math.Random().nextInt(rutas.length)];
+        ruta3 = rutas[math.Random().nextInt(rutas.length)];
+        spins++;
+        if (spins == 10) {
+          timer.cancel();
+          spinning = false;
+        }
+        
+        if (ruta1 == ruta2 && ruta2 == ruta3) {
+          mensaje = 'Ganaste! Felicidades';
+        } else {
+          mensaje = 'Perdiste, vuelve a intentarlo';
+        }
+      });
+    });
   }
 
   @override
