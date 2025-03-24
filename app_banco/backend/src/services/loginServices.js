5f9527a0635aed3864e8ae84037c21a3f20a195b
cmsrwood <dilanfantas@gmail.com>
2025-03-24 14:43:07 -0500

    backend y frontend juntos

---
+++ app_banco/backend/src/services/loginServices.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const ingresarRepository = require('../repository/loginRepository');
+
+
+exports.registrar = async (usuario) => { 
+    const response = await ingresarRepository.registrar(usuario);
+    return response
+}
+
+exports.ingresar = async (usuario) => {
+    const datos = await ingresarRepository.ingresar(usuario);
+
+    if (!datos) { 
+        throw new Error('El usuario no existe')
+    }
+
+    const match = await bcrypt.compare(usuario.password, datos.password);
+    if (!match) { 
+        throw new Error('Contraseña incorrecta')
+    }
+
+    return {
+        message: 'Ingreso exitoso',
+        id: datos.id,
+        nombre: datos.nombre,
+        email: datos.email,
+        token: jwt.sign({ id: datos.id }, 'secret')
+    }
+}
---
+++ app_banco/frontend/lib/hooks/login.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthService {
+  final _storage = FlutterSecureStorage();
+  final String _baseUrl = 'https://localhost:3000';
+
+  Future<bool> login(String email, String password) async {
+    final url = Uri.parse('$_baseUrl/login');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final token =
+          json['token'];
+
+      await _storage.write(key: 'jwt', value: token);
+      return true;
+    } else {
+      // Manejo de error
+      return false;
+    }
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'jwt');
+  }
+
+  Future<void> logout() async {
+    await _storage.delete(key: 'jwt');
+  }
+}
